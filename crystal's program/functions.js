7ec752bb6ec695ff62850f0362ae332669c73be4
“JD <ncsuguy04@gmail.com>
2022-03-04 17:15:11 -0500

    mess with crystals program

--- crystal's program/functions.js
+++ crystal's program/functions.js
@@ -1,5 +1,11 @@
 function helper(l, w, tileSize) {
 
 }
+
+function hide() {
+    let input = document.getElementById('data-inputs');
+    input.style.display = 'block'
+}
+  
 // tilesize 2 element arrray
 // 
